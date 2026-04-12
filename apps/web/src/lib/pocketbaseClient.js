c5d4398d0a82867b01484d74ff483fd17ea99bc1
lizwemtunzib <lizwemtunzib@gmail.com>
2026-04-12 20:25:04 +0400

    fix: use HTTPS for PocketBase URL

--- apps/web/src/lib/pocketbaseClient.js
+++ apps/web/src/lib/pocketbaseClient.js
@@ -2,7 +2,7 @@ import Pocketbase from 'pocketbase';
 
 const POCKETBASE_API_URL = (
     import.meta.env.VITE_POCKETBASE_URL ||
-    'http://pb.carrvin.com'
+    'https://pb.carrvin.com'
 ).replace(/\/$/, '');
 
 const pocketbaseClient = new Pocketbase(POCKETBASE_API_URL);
